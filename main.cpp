16356fe35df273986029936abe5e137a7a6e7940
Stave143 <staveplay@mail.ru>
2025-10-25 18:36:00 +0300

    Added output 1 if cin.err and declair isPithNum

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+bool isPithNum(int a, int b, int c){
+  return 0;
+}
 int main(){
   int a = 0;
   std::cin >> a;
   while(std::cin.good()){
     std::cin >> a;
+  }
+  if(!std::cin.eof()){
+    return 1;
   }
   std::cout << "nice" << "\n";
 }
