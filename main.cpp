b79f2177ba7e6fee7e67481b4a211ed947a54fb9
Stave143 <staveplay@mail.ru>
2025-10-25 18:54:27 +0300

    Intergrate isPithNum in cycle and added last 2 numbers

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 bool isPithNum(int a, int b, int c){
-  return 0;
+  return true;
 }
 int main(){
-  int a = 0;
-  std::cin >> a;
+  int a = 0, b = 0, c = 0;
+  std::cin >> c >> b;
+  int cnt = 0;
   while(std::cin.good()){
+    cnt += isPithNum(a, b, c) ? 1 : 0;
     std::cin >> a;
   }
   if(!std::cin.eof()){
     return 1;
   }
-  std::cout << "nice" << "\n";
+  std::cout << cnt << "\n";
 }
