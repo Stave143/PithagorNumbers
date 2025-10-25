95751ba1693251b60d30401e661ca76eb3184511
Stave143 <staveplay@mail.ru>
2025-10-25 19:08:16 +0300

    Realized isPithNum in Main

--- main.cpp
+++ main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 bool isPithNum(int a, int b, int c){
-  return true;
+  a *= a;
+  b *= b;
+  c *= c;
+  int res = a - b - c;
+  res *= b - a - c;
+  res *= c - a - b;
+  return !res;
 }
 int main(){
   int a = 0, b = 0, c = 0;
   std::cin >> c >> b;
   int cnt = 0;
   while(std::cin.good()){
-    cnt += isPithNum(a, b, c) ? 1 : 0;
     std::cin >> a;
+    cnt += isPithNum(a, b, c) ? 1 : 0;
+    c = b;
+    b = a;
   }
   if(!std::cin.eof()){
     return 1;
